ebfa23694419348829c21fab3eb6eaa1b6e8c60e
Guotrr <631882934@qq.com>
2021-01-18 13:59:24 +0800

    接口工具类

---
+++ src/main/java/com/luqiao/interf/util/ConstantURL.java
@@ -0,0 +1,20 @@
+package com.luqiao.interf.util;
+
+public class ConstantURL {
+
+    public static final String DOMAIN = "http://10.166.130.13:9088";              //用友测试接口
+
+
+    //用友业务单元查询接口
+    public static final String ORGPOST = DOMAIN+ "/service/org/query";
+    //用友部门查询接口
+    public static final String DEPTPOST = DOMAIN+ "/service/dept/query";
+    //用友人员查询接口
+    public static final String PSNDOCPOST = DOMAIN+ "/service/psndoc/query";
+    //用友客商查询接口
+    public static final String CUSTSUPOST = DOMAIN+ "/service/custsup/query";
+    //用友收支项目查询接口
+    public static final String INOUTBUSICLASS = DOMAIN+ "/service/inoutbusiclass/query";
+    //用友核算项目查询接口
+    public static final String PROJECTPOST = DOMAIN+ "/service/project/query";
+}
